147cbcc6e50fd2d7c47d745316bcd74e957ff72d
Rachit Kakkar <rackakka@publicisgroupe.net>
2019-07-31 16:33:29 +0530

    Initial Commit

---
+++ src/com/practice/jdk8/ExceptionPractice2.java
@@ -0,0 +1,27 @@
+package com.practice.jdk8;
+
+public class ExceptionPractice2 {
+	
+	public static void main(String[] args) {
+		
+		try {
+			execute();
+			System.out.println("success");
+			
+		} catch (Exception e) {
+			System.out.println("catch............");
+		}
+	}
+	
+	public static void execute() {
+		try {
+			throw new IllegalArgumentException();
+		} catch (Exception e) {
+			throw new NullPointerException();
+		}
+		finally {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+
+}
